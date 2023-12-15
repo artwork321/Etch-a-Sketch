02fa22981756640d121a016549cfde7f024220db
hakusen <41382001+artwork321@users.noreply.github.com>
2023-12-16 01:08:46 +1100

    add button to adjust the size of canva

--- scripts.js
+++ scripts.js
@@ -3,25 +3,31 @@
 */
 
 const SIZE = 16;
+const MAX_SIZE = 64;
 let canva = document.createElement("div");
 canva.setAttribute("id", "canva");
+let gridNum = SIZE;
 
-for (let i = 0; i < SIZE; i++) {
-    let row = document.createElement("div");
-    row.setAttribute("id", "row");
+function createGrids(gridNum){
+    for (let i = 0; i < gridNum; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("id", "row");
+    
+        for (let i = 0; i < gridNum; i++){
+            grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.addEventListener("mouseover", colored);
+            grid.addEventListener("click", clickColored);
+            row.appendChild(grid);
+        }
+        canva.appendChild(row);
+    } 
 
-    for (let i = 0; i < SIZE; i++){
-        grid = document.createElement("div");
-        grid.classList.add("grid");
-        grid.addEventListener("mouseover", colored);
-        grid.addEventListener("click", clickColored);
-        row.appendChild(grid);
-    }
-    canva.appendChild(row);
-} 
+    document.body.appendChild(canva);
+}
 
+createGrids(gridNum);
 
-document.body.appendChild(canva);
 
 // Event for pressing a grid
 let isMouseDown = false;
@@ -44,4 +50,29 @@ function colored() {
         this.classList.add("active");
     }
 }
+
+/* 
+* Create a button to ask user number of grids to make
+*/
+buttons = document.createElement("div");
+buttons.setAttribute("id", "buttons-container");
+
+numGridButton = document.createElement("button");
+numGridButton.addEventListener("click", takeGrids);
+numGridButton.textContent = "Adjust Size"
+
+buttons.appendChild(numGridButton);
+document.body.insertBefore(buttons, canva);
+
+function takeGrids() {
+    let gridNum = SIZE;
+    gridNum = prompt("Enter the number of grids you want (max 64):");
+
+    if (gridNum > MAX_SIZE)
+        gridNum = MAX_SIZE;
+
+    canva.innerHTML = "";
+    createGrids(gridNum);
+}
+
 
